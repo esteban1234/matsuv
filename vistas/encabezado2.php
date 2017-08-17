50cd965799b728db50a1e8ff1f912508610309dc
esteban1234 <tva_libra@hotmail.com>
2017-08-17 17:44:48 -0500

    meta

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="4Pe3urvM0ON89sX9p8luy5llgWeMO2LO-P4rhW7N7OU" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>MATSUV CONSULTORA Y SOLUCIONES EMPRESARIALES</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
